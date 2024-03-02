784f91187053ce77d102c4991d3c7dda9038de0a
Noushad Chaudhary <139552786+0xNoushad@users.noreply.github.com>
2024-03-02 17:21:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// Cache DOM elements
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobileMenu = document.querySelector('.header .nav-bar .nav-list ul');
+const menuItems = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const toTop = document.querySelector(".to-top");
+
+ 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
 });
 
-var tl = gsap.timeline()
+ 
+document.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
 
-tl.to("#page1",{
-    y:"100vh",
-    scale:0.6,
-    duration:0
-})
-tl.to("#page1",{
-    y:"30vh",
-    duration:1,
-    delay:1
-})
-tl.to("#page1",{
-    y:"0vh",
-    rotate:360,
-    scale:1,
-    duration:0.7
-})
+ 
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
+});
+
+ 
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
+
+ 
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
